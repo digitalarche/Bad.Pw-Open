b261d3c39c4f51af72c33c7c11f8a14bb87e9eef
SpZanG <SpZanG@users.noreply.github.com>
2018-02-10 17:23:58 +1100

    Index Page

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+        <?php include('pass.php'); ?>
+</body>
+</html>
